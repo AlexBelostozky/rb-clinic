1a1861532759a653349ac2e49665482371db11bf
Александр Белостоцкий <belostozky1994@mail.ru>
2023-06-12 01:16:53 +0600

    Add footer desktop styles

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,3 @@
-
-// Swiper script
-
-
-// Imports
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
-//
-// const swiper = new Swiper(".swiper", {
-//   pagination: {
-//     el: ".pagination__page-label",
-//     type: "fraction",
-//   },
-//   navigation: {
-//     nextEl: ".pagination__button--next",
-//     prevEl: ".pagination__button--prev",
-//   },
-// });
-
 // Header navigation logic variables
 const body = document.querySelector('.page__body');
 const navMain = document.querySelector('.header__nav');
