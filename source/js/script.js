1559775054780a089397de5176da603ef07a64b6
Александр Белостоцкий <belostozky1994@mail.ru>
2023-06-12 09:07:31 +0600

    Add slider script

--- source/js/script.js
+++ source/js/script.js
@@ -25,3 +25,62 @@ navToggle.addEventListener('click', function () {
 //
 // Slider
 //
+const slider = document.querySelector('.services');
+const sliderImages = document.querySelectorAll('.services__item');
+const sliderLine = document.querySelector('.services__list');
+
+const sliderBtnNext = document.querySelector('.pagination__button--next');
+const sliderBtnPrev = document.querySelector('.pagination__button--prev');
+
+const paginationCurrent = document.querySelector('.pagination__page-current');
+const paginationAmount = document.querySelector('.pagination__page-amount');
+
+let sliderCount = 0;
+let sliderWidth = slider.offsetWidth;
+
+paginationAmount.textContent = sliderImages.length;
+
+sliderBtnNext.addEventListener('click', goToNextSlide);
+sliderBtnPrev.addEventListener('click', goToPrevSlide);
+window.addEventListener('resize', updateSliderWidth);
+
+function updateSliderWidth () {
+  sliderWidth = slider.offsetWidth;
+  resetSlider();
+}
+
+function resetSlider () {
+  sliderCount = 0;
+  changeSlide();
+}
+
+function goToNextSlide (evt) {
+  evt.preventDefault();
+  sliderCount++;
+
+  if (sliderCount >= sliderImages.length) {
+    sliderCount = 0;
+  }
+
+  changeSlide();
+}
+
+function goToPrevSlide (evt) {
+  evt.preventDefault();
+  sliderCount--;
+
+  if (sliderCount < 0) {
+    sliderCount = sliderImages.length - 1;
+  }
+
+  changeSlide();
+}
+
+function changeSlide () {
+  sliderLine.style.transform = `translateX(${- sliderCount * sliderWidth}px)`;
+  updatePagination();
+}
+
+function updatePagination () {
+  paginationCurrent.textContent = sliderCount + 1;
+}
